d6d9bde9db04609f300ddd35bb3cd4d2d951f60d
edugithub05 <l_aloaxa05@hotmail.com>
2020-05-29 02:07:31 -0500

    últimas actualizaciones hasta el momento

--- docs/src/ts/script.ts
+++ docs/src/ts/script.ts
@@ -1,7 +1,1 @@
 console.log("Primera página en GitHub");
-
-// monaco.editor.create(document.getElementById("mon-editor"),
-// {
-// 	value: "function hello() {\n\talert('Hello world!');\n}",
-// 	language: "javascript"
-// });
